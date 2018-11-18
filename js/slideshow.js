d80ad6df15087e032bc2e849b3990bd76d3cf94d
meow-3 <45090805+meow-3@users.noreply.github.com>
2018-11-18 21:00:31 +0200

    slide

---
+++ js/slideshow.js
@@ -0,0 +1,47 @@
+var countOfSlideShows = 0;
+var slideIndex = [1,1];
+var slideId = [];
+
+function generateSlideShow(id, folder, images, alts)
+{
+	countOfSlideShows++;
+	var mySlides = "mySlides"+countOfSlideShows;
+	for(var j=0; j < countOfSlideShows; j++)
+	{
+		slideId[j] = "mySlides"+(j+1);
+	}
+	var obj = document.getElementById(id);
+	var str = '<h2 style="text-align:center">Slideshow Gallery</h2><div class="container"><div class="numbertext" id="numbertext">-/-</div>';
+	for(var j = 0; j<images.length; j++)
+	{
+		str+='<div class="'+mySlides+'"><img class="slide_img" src="'+folder+'\\'+images[j]+'" alt="'+alts[j]+'"></div>';
+	}
+	str+='<a class="prev" onclick="plusSlides(-1, '+(countOfSlideShows-1)+')">&lt;</a>';
+	str+='<a class="next" onclick="plusSlides(1, '+(countOfSlideShows-1)+')">&gt;</a></div><div class="caption-container"><p id="caption"></p></div>';
+	
+	obj.innerHTML = str;
+	showSlides(1, countOfSlideShows-1);
+}
+
+//
+
+function plusSlides(n, no) {
+  showSlides(slideIndex[no] += n, no);
+}
+
+function showSlides(n, no) {
+  var i;
+  var x = document.getElementsByClassName(slideId[no]);
+  var dots = document.getElementsByClassName("slide_img");
+  var captionText = document.getElementById("caption");
+  var numberText = document.getElementById("numbertext");
+  if (n > x.length) {slideIndex[no] = 1}
+  if (n < 1) {slideIndex[no] = x.length}
+  for (i = 0; i < x.length; i++) {
+	x[i].style.display = "none";
+  }
+  x[slideIndex[no]-1].style.display = "block";
+  captionText.innerHTML = dots[slideIndex[no]-1].alt;
+  
+  numberText.innerHTML = slideIndex[no] + " / " + x.length;
+}
