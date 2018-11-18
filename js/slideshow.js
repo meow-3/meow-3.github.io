c135dace55cd7d7061ea7eb1bd697f02cb0f1db6
meow-3 <45090805+meow-3@users.noreply.github.com>
2018-11-18 21:50:53 +0200

    ідшву

--- js/slideshow.js
+++ js/slideshow.js
@@ -2,7 +2,7 @@ var countOfSlideShows = 0;
 var slideIndex = [1,1];
 var slideId = [];
 
-function generateSlideShow(id, folder, images, alts)
+function generateSlideShow(id, name, folder, images, alt)
 {
 	countOfSlideShows++;
 	var mySlides = "mySlides"+countOfSlideShows;
@@ -11,19 +11,17 @@ function generateSlideShow(id, folder, images, alts)
 		slideId[j] = "mySlides"+(j+1);
 	}
 	var obj = document.getElementById(id);
-	var str = '<h2 style="text-align:center">Slideshow Gallery</h2><div class="container"><div class="numbertext" id="numbertext">-/-</div>';
+	var str = '<h2 style="text-align:center">Slideshow Gallery</h2><div class="container"><div class="numbertext" id="numbertext'+countOfSlideShows+'">1 / '+images.length+'</div>';
 	for(var j = 0; j<images.length; j++)
 	{
-		str+='<div class="'+mySlides+'"><img class="slide_img" src="'+folder+'\\'+images[j]+'" alt="'+alts[j]+'"></div>';
+		str+='<div class="'+mySlides+'"><img class="slide_img slide_img'+countOfSlideShows+'" src="'+folder+'\\'+images[j]+'" alt="'+alt+'"></div>';
 	}
 	str+='<a class="prev" onclick="plusSlides(-1, '+(countOfSlideShows-1)+')">&lt;</a>';
-	str+='<a class="next" onclick="plusSlides(1, '+(countOfSlideShows-1)+')">&gt;</a></div><div class="caption-container"><p id="caption"></p></div>';
+	str+='<a class="next" onclick="plusSlides(1, '+(countOfSlideShows-1)+')">&gt;</a></div><div class="caption-container"><p id="caption'+countOfSlideShows+'"></p></div>';
 	
 	obj.innerHTML = str;
 	showSlides(1, countOfSlideShows-1);
 }
-
-//
 
 function plusSlides(n, no) {
   showSlides(slideIndex[no] += n, no);
@@ -32,9 +30,9 @@ function plusSlides(n, no) {
 function showSlides(n, no) {
   var i;
   var x = document.getElementsByClassName(slideId[no]);
-  var dots = document.getElementsByClassName("slide_img");
-  var captionText = document.getElementById("caption");
-  var numberText = document.getElementById("numbertext");
+  var dots = document.getElementsByClassName("slide_img"+(no+1));
+  var captionText = document.getElementById("caption"+(no+1));
+  var numberText = document.getElementById("numbertext"+(no+1));
   if (n > x.length) {slideIndex[no] = 1}
   if (n < 1) {slideIndex[no] = x.length}
   for (i = 0; i < x.length; i++) {
