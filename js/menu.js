3ba4bf26eabdbf44db52a1535e848980902b3945
meow-3 <45090805+meow-3@users.noreply.github.com>
2018-11-20 02:39:36 +0200

    Update menu.js

--- js/menu.js
+++ js/menu.js
@@ -9,3 +9,44 @@ var sticky = header.offsetTop;
     header.classList.remove("sticky");
   }
 }
+
+function ShowContent(x) {
+	var sub = x.parentElement.getElementsByClassName("Soccer-content")[0];
+	sub.style = "display:block;";
+}
+
+var onSub = 1;
+
+function CheckMenu() {
+	onSub--;
+	var d = document.querySelectorAll( ":hover" );
+	if(d.length > 0)
+	{
+		for(var i=0; i < d.length; i++)
+		{
+			if(d[i].className == "Soccer-content")
+			{
+				onSub = 1;
+			}
+		}
+	}
+	if(onSub < 0)
+	{
+		var subs = document.getElementsByClassName("Soccer-content");
+		for(var i=0; i<subs.length; i++)
+		{
+			subs[i].style = "display:none";
+		}
+		return;
+	}
+	
+	setTimeout(CheckMenu, 1000);
+}
+
+
+
+function HideContent(x) {
+	onSub = 3;
+	setTimeout(CheckMenu, 1000);
+	
+}
