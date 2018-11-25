0337316e53aeff88b3a5f3a9e62deee72f78cd1b
meow-3 <45090805+meow-3@users.noreply.github.com>
2018-11-25 20:57:34 +0200

    upd

--- js/slideshow.js
+++ js/slideshow.js
@@ -24,6 +24,30 @@ function generateSlideShow(id, name, folder, images, alt)
 	showSlides(1, countOfSlideShows-1);
 }
 
+
+
+function generateSlideShowSteply(id, name, folder, images, alts)
+{
+	slideIndex[countOfSlideShows] = 1;
+	countOfSlideShows++;
+	var mySlides = "mySlides"+countOfSlideShows;
+	for(var j=0; j < countOfSlideShows; j++)
+	{
+		slideId[j] = "mySlides"+(j+1);
+	}
+	var obj = document.getElementById(id);
+	var str = '<h2 style="text-align:center">'+name+'</h2><div class="container"><div class="numbertext" id="numbertext'+countOfSlideShows+'">1 / '+images.length+'</div>';
+	for(var j = 0; j<images.length; j++)
+	{
+		str+='<div class="mySlides '+mySlides+'"><img class="slide_img slide_img'+countOfSlideShows+'" src="'+folder+'\\'+images[j]+'" alt="'+alts[j]+'"></div>';
+	}
+	str+='<a class="prev" onclick="plusSlides(-1, '+(countOfSlideShows-1)+')">&lt;</a>';
+	str+='<a class="next" onclick="plusSlides(1, '+(countOfSlideShows-1)+')">&gt;</a></div><div class="caption-container"><p id="caption'+countOfSlideShows+'"></p></div>';
+	
+	obj.innerHTML = str;
+	showSlides(1, countOfSlideShows-1);
+}
+
 function plusSlides(n, no) {
   showSlides(slideIndex[no] += n, no);
 }
